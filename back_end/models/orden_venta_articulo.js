e4ccb5ee90e14e752f4093642ffa14a60feba2b4
mob <marcelobogadop2003@gmail.com>
2025-08-15 17:53:15 -0300

    Cambios en la orden de venta

--- back_end/models/orden_venta_articulo.js
+++ back_end/models/orden_venta_articulo.js
@@ -1,43 +1,47 @@
-import { Sequelize, DataTypes, Op, where } from 'sequelize';
-import { sequelize } from '../config/database.js';
+import { Sequelize, DataTypes, Op, where } from "sequelize";
+import { sequelize } from "../config/database.js";
 
 export const OrdenVentaArticuloSchema = sequelize.define(
-     'OrdenVentaArticulo',
-     {
-          id_orden_venta_articulo: {
-               type: DataTypes.INTEGER,
-               primaryKey: true,
-               autoIncrement: true,
-          },
-          id_orden_venta: {
-               type: DataTypes.INTEGER,
-               allowNull: false,
-          },
-          id_negocio: {
-               type: DataTypes.INTEGER,
-               allowNull: false,
-          },
-          id_articulo: {
-               type: DataTypes.INTEGER,
-               allowNull: false,
-          },
-          precio: {
-               type: DataTypes.INTEGER,
-               allowNull: false,
-          },
-          cantidad: {
-               type: DataTypes.INTEGER,
-               allowNull: false,
-          },
-          nombre_articulo: {
-               type: DataTypes.TEXT,
-               allowNull: false,
-          },
-     },
-     {
-          tableName: 'orden_venta_articulo',
-          timestamps: false,
-     }
+    "OrdenVentaArticulo",
+    {
+        id_orden_venta_articulo: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        id_orden_venta: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        id_negocio: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        id_articulo: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        precio: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        cantidad: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        nombre_articulo: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        imagen: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: "orden_venta_articulo",
+        timestamps: false,
+    }
 );
 
 export class OrdenVentaArticuloModel {}
