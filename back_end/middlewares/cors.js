a73935a99083ad37cf23569e462b6b06efb63bfb
mob <marcelobogadop2003@gmail.com>
2025-11-03 19:25:43 -0300

    Conexion produccion

--- back_end/middlewares/cors.js
+++ back_end/middlewares/cors.js
@@ -1,20 +1,21 @@
-import cors from 'cors';
-import { createRequire } from 'node:module';
+import cors from "cors";
+import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
-const apiUrls = process.env.API_URLS.split(',');
+const apiUrls = process.env.API_URLS.split(",");
 const ACCEPTED_ORIGINS = apiUrls;
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
-     cors({
-          origin: (origin, callback) => {
-               if (acceptedOrigins.includes(origin)) {
-                    return callback(null, true);
-               }
-               if (!origin) {
-                    return callback(null, true);
-               }
-               return callback(new Error('No esta permitido por CORS'));
-          },
-     });
+    cors({
+        // origin: (origin, callback) => {
+        //      if (acceptedOrigins.includes(origin)) {
+        //           return callback(null, true);
+        //      }
+        //      if (!origin) {
+        //           return callback(null, true);
+        //      }
+        //      return callback(new Error('No esta permitido por CORS'));
+        // },
+        origin: "*",
+    });
