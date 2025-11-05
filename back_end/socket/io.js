8066f81258fdd399809f8aa602d5d55c5ff1c65b
mob <marcelobogadop2003@gmail.com>
2025-11-05 19:08:46 -0300

    Correccion a API_URL en produccion

--- back_end/socket/io.js
+++ back_end/socket/io.js
@@ -1,26 +1,27 @@
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 let ioInstance = null;
 
-const apiUrls = process.env.API_URLS.split(',');
+const apiUrls = process.env.API_URLS.split(",");
 const ACCEPTED_ORIGINS = apiUrls;
 
 export const initIO = (server) => {
-     if (!ioInstance) {
-          ioInstance = new Server(server, {
-               cors: {
-                    origin: ACCEPTED_ORIGINS,
-                    methods: ['GET', 'POST'],
-               },
-          });
-     }
-     return ioInstance;
+    if (!ioInstance) {
+        ioInstance = new Server(server, {
+            cors: {
+                // origin: ACCEPTED_ORIGINS,
+                origin: "*",
+                methods: ["GET", "POST"],
+            },
+        });
+    }
+    return ioInstance;
 };
 
 export const getIO = () => {
-     if (!ioInstance) {
-          throw new Error(
-               'Socket.IO no ha sido inicializado. Llama a initIO primero.'
-          );
-     }
-     return ioInstance;
+    if (!ioInstance) {
+        throw new Error(
+            "Socket.IO no ha sido inicializado. Llama a initIO primero."
+        );
+    }
+    return ioInstance;
 };
