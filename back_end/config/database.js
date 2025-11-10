e689b32184e23edef4c0f1d4471de3480c1ee807
mob <marcelobogadop2003@gmail.com>
2025-11-10 19:17:00 -0300

    Cambio de BD a Prod

--- back_end/config/database.js
+++ back_end/config/database.js
@@ -1,28 +1,28 @@
-import { Sequelize } from 'sequelize';
-import { createRequire } from 'node:module';
+import { Sequelize } from "sequelize";
+import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 export const sequelize = new Sequelize(
-     // LOCAL
-     process.env.DATABASE,
-     process.env.DB_USER,
-     process.env.DB_PASSWORD,
-     { host: process.env.HOST, dialect: 'postgres' }
+    // LOCAL
+    // process.env.DATABASE,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    // { host: process.env.HOST, dialect: 'postgres' }
 
-     // // PRODUCCION
-     // process.env.DATABASE_URL_PROD,
-     // {
-     //     dialect: "postgres",
-     //     dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-     // } // Neon requiere SSL
+    // // PRODUCCION
+    process.env.DATABASE_URL_PROD,
+    {
+        dialect: "postgres",
+        dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+    } // Neon requiere SSL
 );
 
 export async function conectarBD() {
-     try {
-          await sequelize.authenticate();
-          console.log(' Conexion a la base de datos exitosa! ');
-     } catch (error) {
-          console.error('Error al conectar con la BD: ', error);
-     }
+    try {
+        await sequelize.authenticate();
+        console.log(" Conexion a la base de datos exitosa! ");
+    } catch (error) {
+        console.error("Error al conectar con la BD: ", error);
+    }
 }
